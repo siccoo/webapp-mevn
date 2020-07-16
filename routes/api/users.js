86e05c2ca2de6971eb4fc0a780649de6e74824e3
siccoo <obichilaka@gmail.com>
2020-07-16 01:57:28 +0100

    creating user route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,2 +1,58 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require(jsonwebtoken);
+const passport = require('passport');
+const User = require('../../model/User');
+
+/**
+ * @route POST api/users/register
+ * @desc Register the user
+ * @access Public
+ **/
+router.post('/register', (req, res) => {
+    let { 
+            name, 
+            username,
+            email, 
+            password, 
+            confirm_password 
+        } = req.body
+    if(password !== confirm_password) {
+        return res.status(400).json({
+            msg: "Password do not match."
+        });
+    }
+
+    // CHECK FOR THE UNIQUE USERNAME
+    User.findOne({
+        username: username
+    }).then(user => {
+        if(user) {
+            return res.status(400).json({
+                msg: "Username is already taken"
+            });
+        }
+    });
+
+    // CHECK FOR THE UNIQUE EMAIL
+    User.findOne({
+        email: email
+    }).then(user => {
+        if(user) {
+            return res.status(400).json({
+                msg: "Email is already registered. Did you forget your password"
+            });
+        }
+    });
+
+    // THE DATA IS VALID AND NEW WE CAN REGISTER THE USER
+    let newUser = new User({
+        name,
+        username,
+        password,
+        email
+    });
+}); 
+
+module.exports = router;
