9566f3337d245f3d723ddd293467dd71d716c3e9
siccoo <obichilaka@gmail.com>
2020-07-16 16:56:40 +0100

    Username and Password in Login.vue

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -3,8 +3,20 @@
     <!-- <h1>Login page</h1> -->
     <div class="row">
       <div class="card mx-auto">
-        <div class="card-header text-white bg-primary">
-
+        <div class="card-header my-5 text-white bg-primary">
+          <h4>LOGIN</h4>
+        </div>
+        <div class="card-body">
+          <form action="">
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" placeholder="Username" class="form-control" name="username" v-model="username" id="username">
+            </div>
+            <div class="form-group">
+              <label for="username">Username</label>
+                <input type="password" placeholder="Password" class="form-control" name="password" v-model="password" id="password">
+            </div>
+          </form>
         </div>
       </div>
     </div>
@@ -14,8 +26,27 @@
 <script>
 
 export default {
-  name: 'Login',
-  components: {
+  data() {
+    return {
+      username: "",
+      password: ""
+    }
   }
 }
 </script>
+
+<style>
+.card {
+  width: 70%;
+  border-radius: none;
+}
+
+.btn {
+  border-radius: none;
+}
+
+.form-control {
+  border-radius: none;
+}
+
+</style>
