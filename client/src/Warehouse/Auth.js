f0e82e3382b43dca018f1824c6821090db2d4fe0
siccoo <obichilaka@gmail.com>
2020-07-17 01:55:50 +0100

    Client authentication

--- client/src/Warehouse/Auth.js
+++ client/src/Warehouse/Auth.js
@@ -21,7 +21,23 @@ const getters = {
 };
 
 const actions = {
-
+// LOGIN ACTION
+async login({ commit }, user){
+    commit('auth_request');
+    let res = await axios.post('https://localhost:5000/api/users/login')
+    if(res.data.success) {
+        const token = res.data.token;
+        const user = res.data.user;
+        
+        // Store the token into the localStorage
+        localStorage.setItem('token', token);
+        
+        // SETTING AXIOS DEFAULT
+        axios.defaults.headers.common['Authorization'] = token;
+        commit('auth_success', token, user);
+    }
+    return res;
+}
 };
 
 const mutations = {
